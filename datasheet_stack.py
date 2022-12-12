25bf4aac72532e73b015fc098b9fb706b627dc25
j.neumeier <jonasneumeier@gmx.de>
2022-12-12 17:01:47 +0100

    updated table rf properties

--- datasheet_stack.py
+++ datasheet_stack.py
@@ -42,7 +42,7 @@ def banner_command():
             r'\hfil \textcolor{white}{\Large \textbf{{#1}}}',
             r'\end{tabular}\end{table}}']
 def space(size):
-    p(r'\vspace{'+size+'}')
+    return [r'\vspace{'+size+'}']
 
 def packages():
     return [r'\usepackage[table]{xcolor}', r'\usepackage{graphicx}', r'\usepackage{setspace}',
@@ -89,23 +89,24 @@ def general_settings():
     pre_writing(logo.command)
     writing(logo.insert())
     writing(Text().toprightcorner())
-def title_text(PM_type, SN):
-    p(r'\phantom{This text will be invisible}')
-    p(r'\vspace{25mm}')
-    p(r'\begin{spacing}{0.5}')
-    p(r'\begin{center} {\huge \textbf{Test Data Sheet} \par}')
-    p(r'\end{center}')
-    p(r'\end{spacing}')
-    p(r'\begin{center} {\Large\textbf{' + PM_type + r'} \par} \end{center}')
-    p(r'\begin{center} {\normalsize ' + SN + ' \par} \end{center}')
-    p(r'\begin{center} {\Large \textbf{Resonant electro-optic phase modulator}}\end{center}')
+def title_text(pm_type, sn):
+    string_list = [r'\phantom{This text will be invisible}',
+                   r'\vspace{25mm}',
+                   r'\begin{spacing}{0.5}',
+                   r'\begin{center} {\huge \textbf{Test Data Sheet} \par}',
+                   r'\end{center}', r'\end{spacing}',
+                   r'\begin{center} {\Large\textbf{' + pm_type + r'} \par} \end{center}',
+                   r'\begin{center} {\normalsize ' + sn + ' \par} \end{center}',
+                   r'\begin{center} {\Large \textbf{Resonant electro-optic phase modulator}}\end{center}']
+    options_list = []
     if options != []:
-        Text('with', r'\normalsize', [0.5, 0.71]).insert()
+        options_list.append(Text('with', r'\normalsize', [0.5, 0.71]).insert()[0])
         inc = 0.0
         for element in options:
             if element in title_options:
                 inc = inc - 0.015
-                Text(r'- '+title_options[element], '', [0.5, 0.71+inc]).insert()
+                options_list.append(Text(r'- '+title_options[element], '', [0.5, 0.71+inc]).insert()[0])
+    return string_list + options_list
 
 
 class Table:
@@ -113,11 +114,11 @@ class Table:
         pass
 
     def __begin(self):
-        p(r'\begin{table}[h]\centering\begin{tabular}{|p{9.5cm}|p{3cm}|p{2.0cm}|}\hline\rowcolor[HTML]{153c4a}\textcolor{white}')
+        return [r'\begin{table}[h]\centering\begin{tabular}{|p{9.5cm}|p{3cm}|p{2.0cm}|}\hline\rowcolor[HTML]{153c4a}\textcolor{white}']
 
     def __end(self):
-        p(r'\end{tabular}\end{table}')
-        p(r'\vspace{-7mm}')
+        return [r'\end{tabular}\end{table}', r'\vspace{-7mm}']
+
     def rf_std(self, f0, bw, q, wl, rf_1rad, RF_max = 0.5):
         if len(wl) > 1:
             rf_1rad = str(rf_1rad[0])+' | '+str(rf_1rad[1])
@@ -125,14 +126,14 @@ class Table:
         else:
             wl = wl[0]
             rf_1rad = str(rf_1rad[0])
-        self.__begin()
-        p(r'{\textbf{RF properties}}  & \hfil \textcolor{white}{\textbf{Value}} & \hfil \textcolor{white}{\textbf{Unit}}  \\ \hline')
-        p(r'Resonance frequency: f$_{0}$ $^{1)}$  & \hfil '+str(f0[0])+r' & \hfil '+f0[1]+r' \\ \hline')
-        p(r'Bandwidth: $\Delta \nu$  & \hfil '+str(bw[0])+r'   & \hfil '+bw[1]+r' \\ \hline')
-        p(r'Quality Factor: Q & \multicolumn{2}{|c|}{'+str(q)+r'}  \\ \hline')
-        p(r'Required RF power for 1rad $@$ '+str(wl)+r'nm $^{2)}$   & \hfil '+rf_1rad+r' & \hfil dBm \\ \hline')
-        p(r'max. RF power: RF\textsubscript{max} $^{3)}$ & \hfil '+str(RF_max)+r'   & \hfil W \\ \hline')
-        self.__end()
+
+        return self.__begin()+[r'{\textbf{RF properties}}  & \hfil \textcolor{white}{\textbf{Value}} & \hfil \textcolor{white}{\textbf{Unit}}  \\ \hline',
+                        r'Resonance frequency: f$_{0}$ $^{1)}$  & \hfil '+str(f0[0])+r' & \hfil '+f0[1]+r' \\ \hline',
+                        r'Bandwidth: $\Delta \nu$  & \hfil ' + str(bw[0]) + r'   & \hfil ' + bw[1] + r' \\ \hline',
+                        r'Quality Factor: Q & \multicolumn{2}{|c|}{' + str(q) + r'}  \\ \hline',
+                        r'Required RF power for 1rad $@$ ' + str(wl) + r'nm $^{2)}$   & \hfil ' + rf_1rad + r' & \hfil dBm \\ \hline',
+                        r'max. RF power: RF\textsubscript{max} $^{3)}$ & \hfil ' + str(RF_max) + r'   & \hfil W \\ \hline']+self.__end()
+
     def RF_tuning(self):
         pass
     def optical_std(self,aperture,wavefront,wl,intensity,r_ar,ar):
@@ -182,8 +183,8 @@ def get_RF_1rad(pow_dBm_1rad, wl_):
             rf_1rad.append(pow_dBm_1rad[key])
     return rf_1rad
 
-path = "P:/Ablage/j.neumeier/aktuelleProduktion/5F_L3x3x30-NIR test/"
-type, options, aperture, wl, wavefront = auftrag(file='PO/ProdAuftrag_T_TC.pdf', pos=1)
+path = r"P:/Ablage/j.neumeier/aktuelleProduktion/5F_L3x3x30-NIR test/"
+pm_type, options, aperture, wl, wavefront = auftrag(file='PO/ProdAuftrag_T_TC.pdf', pos=1)
 title_options = {'+TXC': 'Temperature control option', '+TC': 'Temperature control option', '+T1': 'Frequency tuning option',
                  '+T': 'Frequency tuning option', '+W': 'Crystal wedge option', '+DC': 'DC-port option'}
 vna = VNA(path + 'vna_remote.txt')
@@ -193,12 +194,11 @@ rf_1rad_values = get_RF_1rad(power_dbm_1rad(path + 'measuredmodulation.pdf'), wl
 
 
 def fill_document():
-    doc.append(r'asd')
     general_settings()
-    # title_text(PM_type=type, SN='SN22.1235')
+    writing(title_text(pm_type=pm_type, sn='SN22.1235'))
     # Picture('Title', 'images/Cube_page1.pdf', [0, 45], '4.0cm').insert()
-    # space('70mm')
-    # Table().rf_std(f0=vna.f0('MHz'), bw=vna.bw('kHz'), q=vna.q(), wl=wl, rf_1rad=rf_1rad_values)
+    writing(space('70mm'))
+    writing(Table().rf_std(f0=vna.f0('MHz'), bw=vna.bw('kHz'), q=vna.q(), wl=wl, rf_1rad=rf_1rad_values))
     # Table().optical_std(aperture=aperture, wavefront=wavefront, wl=wl, intensity=1, r_ar=1, ar='630 - 1100') #take ar from .csv where label info, SN, tuning, etc
     # footnote_page1(T=23, damage=1)
     # measured_modulation()
