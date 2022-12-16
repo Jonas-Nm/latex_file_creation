2e7b9bb08c907d256292dbf3e9fe636c368f9572
Jonas <jonas.neumeier@mpq.mpg.de>
2022-12-16 14:39:17 +0100

    added new pdf scraping for beta app

--- pdfscraping.py
+++ pdfscraping.py
@@ -1,7 +1,23 @@
 from PyPDF2 import PdfFileReader
 import re
 import os
+from tika import parser  #needs java run time
 
+
+file = r'C:\Users\Jonas\PycharmProjects\latex_file_creation\important_files\mod_betaapp.pdf'
+#data = tb.read_pdf(file, area = (300, 0, 600, 800), pages = '1')
+parsedPDF = parser.from_file(file)
+content = parsedPDF['content']
+print(content)
+wl = re.search('nm (.*)\n', content).group(1)
+power = re.search('P dBm (.*)\n', content).group(1)
+wl_measured = re.search('(.*) \(meas\.\)', content).group(1)
+print(wl)
+print(power)
+print(wl_measured)
+#dfs = tb.read_pdf(file, pages='all')
+#print(dfs)
+#print(type(dfs))
 
 def power_dbm_1rad(file):
     pdftext = PdfFileReader(file).pages[0].extract_text()
@@ -16,7 +32,7 @@ def power_dbm_1rad(file):
     return P_dBm_1rad
 
 #path = r'P:\Ablage\j.neumeier\aktuelleProduktion\Cold Quanta\4T_M3x3x15-NIR_W SN22.0375R Cold Quanta'.replace('\\', '/')
-#power_dbm_1rad(os.path.join(path, 'mod.pdf'))
+#power_dbm_1rad(file)
 
 def auftrag(file, pos = 1):
     pdf = PdfFileReader(file)
